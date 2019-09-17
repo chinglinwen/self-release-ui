21d1359507e11e7cc6c99156999584cb1cb930f9
wenzhenglin <wenzhenglin@haodai.net>
2019-09-17 16:22:10 +0800

    fix update check issue

--- src/components/ConfigProject.vue
+++ src/components/ConfigProject.vue
@@ -1,91 +1,88 @@
 <template>
-  <v-row justify="center">
-    <v-dialog
-      v-model="dialog"
-      persistent
-      max-width="600px"
-      fullscreen
-      hide-overlay
-      transition="dialog-bottom-transition"
-    >
-      <template v-slot:activator="{ on }">
-        <v-btn left color="primary" dark v-on="on" @click="opensetting">Set</v-btn>
-      </template>
+  <!-- <v-row justify="center"> -->
+  <!-- <v-row >  -->
+  <v-dialog
+    justify="center"
+    v-model="dialog"
+    persistent
+    max-width="600px"
+    fullscreen
+    hide-overlay
+    transition="dialog-bottom-transition"
+  >
+    <template v-slot:activator="{ on }">
+      <v-btn left color="primary" dark v-on="on" @click="opensetting">Set</v-btn>
+    </template>
 
-      <v-card>
-        <v-btn @click="dialog = false">
-          <v-icon>arrow_back_ios</v-icon>
-        </v-btn>
-        <v-card-title outline>
-          <span class="headline">Resource Binding for project: {{ project.name }}</span>
-        </v-card-title>
+    <v-card>
+      <v-btn @click="dialog = false">
+        <v-icon>arrow_back_ios</v-icon>
+      </v-btn>
+      <v-card-title outline>
+        <span class="headline">Resource Binding for project: {{ project.name }}</span>
+      </v-card-title>
 
-        <v-tabs v-model="tab" background-color="deep-purple accent-2" class="elevation-0" dark>
-          <v-tab
-            v-for="env in envlist"
-            :key="env"
-            @click="changeTab(env)"
-            :href="`#${env}`"
-          >{{ env }}</v-tab>
+      <v-tabs v-model="tab" background-color="deep-purple accent-2" class="elevation-0" dark>
+        <v-tab v-for="env in envlist" :key="env" @click="changeTab(env)" :href="`#${env}`">{{ env }}</v-tab>
 
-          <v-tab-item v-for="env in envlist" :key="env" :value="env">
-            <v-card-text>
-              <Env :existItems="x.existEnvs" @add:item="envsSubmit" @delete:item="envsDelete" />
+        <v-tab-item v-for="env in envlist" :key="env" :value="env">
+          <v-card-text>
+            <Env :existItems="x.existEnvs" @add:item="envsSubmit" @delete:item="envsDelete" />
 
-              <Mysql
-                :existItems="x.existMysql"
-                :items="mysqlinfo"
-                @add:item="mysqlSubmit"
-                @delete:item="mysqlDelete"
-              />
+            <Mysql
+              :existItems="x.existMysql"
+              :items="mysqlinfo"
+              @add:item="mysqlSubmit"
+              @delete:item="mysqlDelete"
+            />
 
-              <Redis
-                :existItems="x.existRedis"
-                :items="redisinfo"
-                @add:item="redisSubmit"
-                @delete:item="redisDelete"
-              />
+            <Redis
+              :existItems="x.existRedis"
+              :items="redisinfo"
+              @add:item="redisSubmit"
+              @delete:item="redisDelete"
+            />
 
-              <Nfs
-                :existItems="x.existNfs"
-                :items="nfsinfo"
-                @add:item="nfsSubmit"
-                @delete:item="nfsDelete"
-              />
+            <Nfs
+              :existItems="x.existNfs"
+              :items="nfsinfo"
+              @add:item="nfsSubmit"
+              @delete:item="nfsDelete"
+            />
 
-              <v-card-actions>
-                <v-btn color="blue darken-1" text @click="dialog = false;loading=false">Close</v-btn>
-                <v-btn :disabled="submitting" color="blue darken-1" text @click="submitall">Save All</v-btn>
-              </v-card-actions>
+            <v-card-actions>
+              <v-btn color="blue darken-1" text @click="dialog = false;loading=false">Close</v-btn>
+              <v-btn :disabled="submitting" color="blue darken-1" text @click="submitall">Save All</v-btn>
+            </v-card-actions>
 
-              <v-progress-linear
-                v-if="loading"
-                color="blue accent-4"
-                indeterminate
-                rounded
-                height="2"
-              ></v-progress-linear>
+            <v-progress-linear
+              v-if="loading"
+              color="blue accent-4"
+              indeterminate
+              rounded
+              height="2"
+            ></v-progress-linear>
 
-              <v-flex ma="5">
-                <v-snackbar
-                  v-if="notify"
-                  v-model="notify"
-                  :bottom="true"
-                  :color="notify.color"
-                  :multi-line="true"
-                  :right="true"
-                  :timeout="notify.timeout"
-                >
-                  {{ notify.msg }}
-                  <v-btn pa="10" dark text @click="notify = false">Close</v-btn>
-                </v-snackbar>
-              </v-flex>
-            </v-card-text>
-          </v-tab-item>
-        </v-tabs>
-      </v-card>
-    </v-dialog>
-  </v-row>
+            <v-flex ma="5">
+              <v-snackbar
+                v-if="notify"
+                v-model="notify"
+                :bottom="true"
+                :color="notify.color"
+                :multi-line="true"
+                :right="true"
+                :timeout="notify.timeout"
+              >
+                {{ notify.msg }}
+                <v-btn pa="10" dark text @click="notify = false">Close</v-btn>
+              </v-snackbar>
+            </v-flex>
+          </v-card-text>
+        </v-tab-item>
+      </v-tabs>
+    </v-card>
+  </v-dialog>
+  <!-- </v-row> -->
 </template>
 
 <script>
@@ -108,41 +105,27 @@ export default {
     }
   },
   data: () => ({
-    // mysql: {},
-
     dialog: false,
-
     notify: null,
     //deploy env
     tab: "online",
     envlist: ["online", "pre", "test"],
-
     x: {
       existMysql: {},
       existEnvs: {},
       existRedis: {},
-      existNfs: {},
-      _existMysql: {},
-      _existEnvs: {},
-      _existRedis: {},
-      _existNfs: {}
+      existNfs: {}
     },
-    _x: {},
-
     all: {
       online: {},
       pre: {},
       test: {}
     },
-    // existResource: {},
-
-    // cache
-
-    // see if need update
-    updated: false,
+    // cache to compare if updated
+    _all: {},
+    // form status
     loading: false,
     submitting: false,
-
     infos: {},
     // envsinfo: [],
     mysqlinfo: [],
@@ -170,8 +153,6 @@ export default {
 
       this.x = this.all[env];
       console.log("got x: ", this.x, "for env: ", env);
-
-      this._x = Object.assign({}, this.x);
     },
     opensetting() {
       this.getInfo();
@@ -189,7 +170,8 @@ export default {
       this.all.pre = x;
       this.all.test = x;
       this.x = x;
-      this._x = Object.assign({}, this.x);
+      // this._all = Object.assign({}, this.all);
+      this._all = JSON.parse(JSON.stringify(this.all));
 
       fetch(
         "http://192.168.10.234:8089/api/projects/" +
@@ -204,7 +186,9 @@ export default {
           this.all.pre = convert(this.resources["pre"]);
           this.all.test = convert(this.resources["test"]);
           this.x = this.all.online;
-          this._x = Object.assign({}, this.x);
+
+          // deep copy instead
+          this._all = JSON.parse(JSON.stringify(this.all));
         })
         .catch(error => {
           console.log("get resource api err", error);
@@ -218,80 +202,46 @@ export default {
     },
     mysqlSubmit(item) {
       console.log("add mysql", item);
-
-      if (
-        JSON.stringify(this.x._existMysql) == JSON.stringify(this.x.existMysql)
-      ) {
-        console.log("no need update mysql");
-      }
       let a = this.x.existMysql.find(e => e.id === item.id);
       if (!a) {
         this.x.existMysql.push(Object.assign({}, item));
       }
-      a = item;
-      this.updated = true;
-      this.x._existMysql = this.x.existMysql;
     },
     envsDelete(item) {
       this.x.existEnvs = this.x.existEnvs.filter(value => {
         return value.id != item.id;
       });
-      this.updated = true;
     },
     envsSubmit(item) {
       console.log("add envs", item);
-      if (
-        JSON.stringify(this.x._existEnvs) == JSON.stringify(this.x.existEnvs)
-      ) {
-        console.log("no need update envs");
-      }
       let a = this.x.existEnvs.find(e => e.id === item.id);
       if (!a) {
         this.x.existEnvs.push(Object.assign({}, item));
       }
-      a = item;
-      this.updated = true;
-      this.x._existEnvs = this.x.existEnvs;
     },
     redisDelete(item) {
       this.x.existRedis = this.x.existRedis.filter(value => {
         return value.id != item.id;
       });
-      this.updated = true;
     },
     redisSubmit(item) {
       console.log("add redis", item);
-      if (
-        JSON.stringify(this.x._existRedis) == JSON.stringify(this.x.existRedis)
-      ) {
-        console.log("no need update redis");
-      }
       let a = this.x.existRedis.find(e => e.id === item.id);
       if (!a) {
         this.x.existRedis.push(Object.assign({}, item));
       }
-      a = item;
-      this.updated = true;
-      this.x._existRedis = this.x.existRedis;
     },
     nfsDelete(item) {
       this.x.existNfs = this.x.existNfs.filter(value => {
         return value.id != item.id;
       });
-      this.updated = true;
     },
     nfsSubmit(item) {
       console.log("add nfs", item);
-      if (JSON.stringify(this.x._existNfs) == JSON.stringify(this.x.existNfs)) {
-        console.log("no need update nfs");
-      }
       let a = this.x.existNfs.find(e => e.id === item.id);
       if (!a) {
         this.x.existNfs.push(Object.assign({}, item));
       }
-      a = item;
-      this.updated = true;
-      this.x._existNfs = this.x.existNfs;
     },
     getInfo() {
       console.log("fetching info in the background");
@@ -320,67 +270,21 @@ export default {
         });
       return;
     },
-    // should be api call
-    getResource(project) {
-      // let namewithpath = this.project.name.split("/");
-      // let ns = namewithpath[0];
-
-      // const json = await fetch(
-      //   "http://192.168.10.234:8089/api/projects/" +
-      //     this.project.name +
-      //     "/values"
-      // ).then(response => response.json());
-      // console.log(json);
-      // debugger;
-      // return json;
-
-      // // let a = {};
-      // return fetch(
-      //   "http://192.168.10.234:8089/api/projects/" +
-      //     this.project.name +
-      //     "/values"
-      // ).then(response => response.json());
-
-      // .then(json => {
-      //   // this.resources = json;
-      //   a = json;
-      //   console.log("get resource: ", json);
-      // })
-      // .catch(error => {
-      //   console.log("get resource err", error);
-      // });
-
-      // let a = this.$GET(
-      //   "http://192.168.10.234:8089/api/projects/" +
-      //     this.project.name +
-      //     "/values"
-      // )
-      //   .then(res => {
-      //     this.resources = res;
-      //   })
-      //   .catch(e => {
-      //     console.log("getResource err", e);
-      //   });
-
-      // "http://192.168.10.234:8089/api/resources/xindaiquan"
-      // return json array
-      return JSON.parse(_existResource);
-      // debugger;
-      // return a.data;
-    },
     submitall() {
+      if (JSON.stringify(this._all) == JSON.stringify(this.all)) {
+        this.notify = { color: "orange", msg: "there's no change" };
+        console.log("no need update all");
+        return;
+      }
+      // prevent submitting during submitting
+      this.submitting = true;
+
       let a = convertback(this.all);
       let url =
         "http://192.168.10.234:8089/api/projects/" +
         this.project.name +
         "/values";
       let data = JSON.stringify(a, replacer, 2);
-
-      if (!this.updated) {
-        this.notify = { color: "orange", msg: "there's no change" };
-        return;
-      }
-      this.submitting = true;
 
       // call api
       this.loading = true;
@@ -396,7 +300,8 @@ export default {
             msg: "all saved",
             timeout: 50000
           };
-          this._x = this.x;
+          // deep copy instead
+          this._all = JSON.parse(JSON.stringify(this.all));
         })
         .catch(err => {
           console.log("submit all err", err);
