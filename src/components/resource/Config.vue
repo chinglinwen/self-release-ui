08335f8faa88aa1b9b8fd7fb70feb15946a6c63d
wenzhenglin <wenzhenglin@haodai.net>
2019-09-25 14:31:47 +0800

    update config validate

--- src/components/resource/Config.vue
+++ src/components/resource/Config.vue
@@ -2,56 +2,58 @@
   <div>
     <v-card-title>Configs</v-card-title>
     <v-container v-if="config" width="600px">
-      <v-layout>
-        <v-row>
-          <v-flex v-if="env=='online'" md2 pa-2>
-            <div v-if="editing === item.id">
-              NodePort:
-              <v-text-field label v-model="config.nodePort"></v-text-field>
-            </div>
-            <div v-else>NodePort: {{ config.nodePort }}</div>
-          </v-flex>
-          <v-flex v-if="env=='online'" md2 pa-2>
-            <div v-if="editing === item.id">
-              Domain:
-              <v-text-field label v-model="config.domain"></v-text-field>
-            </div>
-            <div v-else>Domain: {{ config.domain }}</div>
-          </v-flex>
-          <v-flex v-if="env=='online'" md2 pa-2>
-            <div v-if="editing === item.id">
-              Monitor address:
-              <v-text-field label v-model="config.monitor.address"></v-text-field>
-            </div>
-            <div v-else>Monitor address: {{ config.monitor.address }}</div>
-          </v-flex>
+      <v-form ref="form" v-model="valid">
+        <v-layout @change="formIsValid(config)">
+          <v-row>
+            <v-flex v-if="env=='online'" md2 pa-2>
+              <div v-if="editing === item.id">
+                NodePort:
+                <v-text-field label :rules="rules.notzero" v-model="config.nodePort"></v-text-field>
+              </div>
+              <div v-else>NodePort: {{ config.nodePort }}</div>
+            </v-flex>
+            <v-flex v-if="env=='online'" md2 pa-2>
+              <div v-if="editing === item.id">
+                Domain:
+                <v-text-field label :rules="rules.notempty" v-model="config.domain"></v-text-field>
+              </div>
+              <div v-else>Domain: {{ config.domain }}</div>
+            </v-flex>
+            <v-flex v-if="env=='online'" md2 pa-2>
+              <div v-if="editing === item.id">
+                Monitor address:
+                <v-text-field label :rules="rules.notempty" v-model="config.monitor.address"></v-text-field>
+              </div>
+              <div v-else>Monitor address: {{ config.monitor.address }}</div>
+            </v-flex>
 
-          <v-flex v-if="config.deploy" md2 pa-2>
-            <div v-if="editing === item.id">
-              Replicas:
-              <v-text-field label v-model="config.deploy.replicas"></v-text-field>
-            </div>
-            <div v-else>Replicas: {{ config.deploy.replicas }}</div>
-          </v-flex>
+            <v-flex v-if="config.deploy" md2 pa-2>
+              <div v-if="editing === item.id">
+                Replicas:
+                <v-text-field label :rules="rules.notzero" v-model="config.deploy.replicas"></v-text-field>
+              </div>
+              <div v-else>Replicas: {{ config.deploy.replicas }}</div>
+            </v-flex>
 
-          <div v-if="editing === item.id">
-            <v-btn @click="cancelEdit(config)">
-              <!-- <v-icon outline>cancel</v-icon> -->
-              cancel
-            </v-btn>
-            <v-btn :disabled="!formIsValid(config)" @click="handleSubmit(config)">
-              <!-- <v-icon>save</v-icon> -->
-              save
-            </v-btn>
-          </div>
-          <div v-else>
-            <v-btn @click="editMode(config)">
-              <!-- <v-icon>edit</v-icon> -->
-              edit
-            </v-btn>
-          </div>
-        </v-row>
-      </v-layout>
+            <div v-if="editing === item.id">
+              <v-btn @click="cancelEdit(config)">
+                <!-- <v-icon outline>cancel</v-icon> -->
+                cancel
+              </v-btn>
+              <v-btn :disabled="!valid" @click="handleSubmit(config)">
+                <!-- <v-icon>save</v-icon> -->
+                save
+              </v-btn>
+            </div>
+            <div v-else>
+              <v-btn @click="editMode(config)">
+                <!-- <v-icon>edit</v-icon> -->
+                edit
+              </v-btn>
+            </div>
+          </v-row>
+        </v-layout>
+      </v-form>
     </v-container>
     <v-container v-else>fetching...</v-container>
   </div>
@@ -73,6 +75,22 @@ export default {
     submitting: false,
     success: false,
 
+    valid: true,
+    rules: {
+      notempty: [
+        v => !!v || "must not empty",
+        // v => v != "0" || "must not zero",
+        v => v === "" && "must not empty string",
+        v => /^\w+$/.test(v) || "must be string",
+        v => !/^\d+$/.test(v) || "must not be number",
+        v => v.length <= 2 && "must be more than 2 characters"
+      ],
+      notzero: [
+        v => !!v || "must not empty",
+        v => v === "0" && "must not zero",
+        v => /^\d+$/.test(v) || "must be number"
+      ]
+    },
     creating: null,
     editing: null,
     search: "",
@@ -89,72 +107,52 @@ export default {
     itemname: ""
   }),
   created() {
-    console.log("config env: ", this.env, this.existConfig);
+    // console.log("config env: ", this.env, this.existConfig);
+  },
+  watch: {
+    env: function(v, old) {
+      console.log("new env", v, "old", old);
+      this.clearStatus();
+    }
   },
   methods: {
-    formIsValid(item) {
-      debugger;
-      if (this.env == "online") {
-        return (
-          item.nodePort != "0" &&
-          item.domain != "" &&
-          item.deploy.replicas != "0" &&
-          item.monitor.address != ""
-        );
-      }
-      if (this.env == "pre") {
-        return item.deploy.replicas != "0";
-      }
-      if (this.env == "test") {
-        return item.deploy.replicas != "0";
-      }
-      // return false;
-    },
-    checkitem(item) {
-      console.log("show detail div", this.itemname);
-      return this.itemname != undefined && this.itemname != "";
-    },
+    // formIsValid(item) {
+    //   if (this.env == "online") {
+    //     if (
+    //       item.nodePort != "0" &&
+    //       item.domain != "" &&
+    //       item.deploy.replicas != "0" &&
+    //       item.monitor.address != ""
+    //     );
+    //   }
+    //   if (this.env == "pre") {
+    //     return item.deploy.replicas != "0";
+    //   }
+    //   if (this.env == "test") {
+    //     return item.deploy.replicas != "0";
+    //   }
+    //   // return false;
+    // },
     editMode(item) {
       this.cacheditem = Object.assign({}, item);
       this.editing = item.id;
       this.creating = false;
     },
-
     cancelEdit(item) {
       Object.assign(item, this.cacheditem);
-      this.editing = null;
-    },
-    cancelCreate(item) {
-      this.creating = false;
-    },
-
-    editItem(item) {
-      if (item.id === "" || item.host === "") return;
-      this.$emit("edit:item", item.id, item);
-      this.editing = null;
-    },
-    deleteItem(item) {
-      if (item.id === "") return;
-      console.log("delete", item);
-      this.$emit("delete:item", item);
       this.editing = null;
     },
 
     // form
     handleSubmit(item) {
-      this.clearStatus();
-      this.submitting = true;
-      // if (!update) {
-      //   item.id = 1;
-      //   if (this.existItems.length != 0) {
-      //     item.id = this.existItems[this.existItems.length - 1].id + 1;
-      //   }
+      // if (this.$refs.form.validate()) {
+      //   this.snackbar = true;
+      //   console.log("not valid for config");
       // }
 
-      // if (item.host === "") {
-      //   this.error = true;
-      //   return;
-      // }
+      this.clearStatus();
+      this.submitting = true;
+
       item.nodePort = parseInt(item.nodePort, 10);
       item.deploy.replicas = parseInt(item.deploy.replicas, 10);
       this.$emit("add:item", item);
