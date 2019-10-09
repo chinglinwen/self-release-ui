7a049cd02e99494765819d5b0a3e1aec14ad9ac8
wenzhenglin <wenzhenglin@haodai.net>
2019-10-09 17:31:24 +0800

    update projectconfig

--- src/components/ProjectConfig.vue
+++ src/components/ProjectConfig.vue
@@ -9,161 +9,97 @@
         <v-card-title>Project's Configs</v-card-title>
         <v-container v-if="config">
           <v-form ref="form" v-model="valid">
-            <!-- <v-content> -->
-            <v-layout column>
-              <v-flex>
-                <div ma="12" pa="2">
-                  Dev Branch:
-                  <v-text-field
-                    v-if="editing === config.id"
-                    label
-                    :rules="rules.notzero"
-                    v-model="config.devBranch"
-                  ></v-text-field>
-                  <div v-else>{{ config.devBranch }}</div>
-                </div>
-              </v-flex>
-              <v-flex>
-                <div ma="12" pa="2">
-                  Build Mode:
-                  <v-text-field
-                    v-if="editing === config.id"
-                    label
-                    :rules="rules.notzero"
-                    v-model="config.buildmode"
-                  ></v-text-field>
-                  <div v-else>{{ config.buildmode }}</div>
-                </div>
-              </v-flex>
-              <v-flex>
-                <div ma="12" pa="2">
-                  Config Version:
-                  <v-text-field
-                    v-if="editing === config.id"
-                    label
-                    :rules="rules.notzero"
-                    v-model="config.configver"
-                  ></v-text-field>
-                  <div v-else>{{ config.configver }}</div>
-                </div>
-              </v-flex>
-
-              <v-flex>
-                <div ma="12" pa="2">
-                  Version:
-                  <v-text-field
-                    v-if="editing === config.id"
-                    label
-                    placeholder="v1.0.0"
-                    :rules="rules.notzero"
-                    v-model="config.version"
-                  ></v-text-field>
-                  <div v-else>{{ config.version }}</div>
-                </div>
-              </v-flex>
-            </v-layout>
-            <!-- </v-content> -->
-            <!-- <v-row>
-              <v-span>devBranch:</v-span>
-              <v-div
-                class="d-inline"
-                deep-purple
-                accent-4
-                white--text
-                v-if="editing === config.id"
-                pa="0"
-                ma="0"
-              >
-                <span>Enable:</span>
-                <v-text-field label :rules="rules.notzero" v-model="config.devBranch"></v-text-field>
-              </v-div>
-              <v-div v-else>{{ config.devBranch }}</v-div>
-            </v-row>-->
-
-            <!-- <v-row>
-              <v-div class="d-block pa-2" v-if="editing === config.id" md2 pa-2>
-                <v-text-field
-                  class="d-inline"
-                  label="Enable:"
-                  :rules="rules.notzero"
-                  v-model="config.enable"
-                ></v-text-field>
-              </v-div>
-              <v-div v-else class="d-block pa-2" md2 pa-2>Enable: {{ config.enable }}</v-div>
-            </v-row>
-            <v-row>
-              <v-div class="d-block pa-2" v-if="editing === config.id" md2 pa-2>
-                <v-text-field
-                  class="d-inline"
-                  label="Enable:"
-                  :rules="rules.notzero"
-                  v-model="config.enable"
-                ></v-text-field>
-              </v-div>
-              <v-div v-else class="d-block pa-2" md2 pa-2>Enable: {{ config.enable }}</v-div>
-            </v-row>
-            <v-row>-->
-            <!-- <v-col v-if="editing === config.id" md2 pa-2>
+            <div v-if="editing === config.id">
+              <v-layout column>
+                <v-flex>
                   Enable:
-                  <v-text-field label :rules="rules.notzero" v-model="config.enable"></v-text-field>
-                </v-col>
-                <v-col v-else md2 pa-2>Enable: {{ config.enable }}</v-col>
+                  <v-switch v-model="config.enable" @click="setdefault"></v-switch>
+                </v-flex>
 
-                <v-col v-if="editing === config.id" md2 pa-2>
-                  Enable:
-                  <v-text-field label :rules="rules.notzero" v-model="config.enable"></v-text-field>
-                </v-col>
-            <v-col v-else md2 pa-2>Enable: {{ config.enable }}</v-col>-->
-            <!-- </v-row> -->
-            <!-- <v-row v-if="editing === config.id">
-              devBranch:
-              <v-text-field label :rules="rules.notzero" v-model="config.devBranch"></v-text-field>
-            </v-row>
-            <v-row v-else>devBranch: {{ config.devBranch }}</v-row>
+                <v-flex>
+                  <div ma="12" pa="2">
+                    Dev Branch:
+                    <v-text-field v-if="editing === config.id" label v-model="config.devBranch"></v-text-field>
+                  </div>
+                </v-flex>
+                <v-flex>
+                  <div ma="12" pa="2">
+                    Build Mode:
+                    <!-- <v-text-field v-if="editing === config.id" label v-model="config.buildMode"></v-text-field> -->
+                    <v-select
+                      v-if="editing === config.id"
+                      v-model="config.buildMode"
+                      :items="buildmodes"
+                      label
+                    ></v-select>
+                  </div>
+                </v-flex>
+                <v-flex>
+                  <div ma="12" pa="2">
+                    K8s Template Version:
+                    <!-- <v-text-field v-if="editing === config.id" label v-model="config.configVer"></v-text-field> -->
 
-            <v-row>
-              <v-col v-if="editing === config.id" md2 pa-2>
-                buildMode:
-                <v-text-field label :rules="rules.notzero" v-model="config.buildMode"></v-text-field>
-              </v-col>
-              <v-col v-else md2 pa-2>buildMode: {{ config.buildMode }}</v-col>
+                    <v-select
+                      v-if="editing === config.id"
+                      v-model="config.configVer"
+                      :items="configvers"
+                      label
+                    ></v-select>
+                  </div>
+                </v-flex>
 
-              <v-col v-if="editing === config.id" md2 pa-2>
-                configVer:
-                <v-text-field label :rules="rules.notzero" v-model="config.configVer"></v-text-field>
-              </v-col>
-              <v-col v-else md2 pa-2>configVer: {{ config.configVer }}</v-col>
+                <v-flex>
+                  <div ma="12" pa="2">
+                    Self-Release Version:
+                    <!-- <v-text-field v-if="editing === config.id" label v-model="config.version"></v-text-field> -->
+                    <v-select
+                      v-if="editing === config.id"
+                      v-model="config.version"
+                      :items="vers"
+                      label
+                    ></v-select>
+                  </div>
+                </v-flex>
+              </v-layout>
+            </div>
+            <!-- <div v-else>
+              <v-layout column>
+                <v-flex md="6">Enable: {{ config.devBranch }}</v-flex>
 
-              <v-col v-if="editing === config.id" md2 pa-2>
-                version:
-                <v-text-field label :rules="rules.notzero" v-model="config.version"></v-text-field>
-              </v-col>
-              <v-col v-else md2 pa-2>version: {{ config.version }}</v-col>\
-            </v-row>-->
+                <v-flex md="6">Dev Branch: {{ config.devBranch }}</v-flex>
 
-            <!-- </v-flex> -->
-            <!-- <v-flex v-if="env=='online'" md2 pa-2>
-                <div v-if="editing === item.id">
-                  Domain:
-                  <v-text-field label :rules="rules.notempty" v-model="config.domain"></v-text-field>
-                </div>
-                <div v-else>Domain: {{ config.domain }}</div>
-              </v-flex>
-              <v-flex v-if="env=='online'" md2 pa-2>
-                <div v-if="editing === item.id">
-                  Monitor address:
-                  <v-text-field label :rules="rules.notempty" v-model="config.monitor.address"></v-text-field>
-                </div>
-                <div v-else>Monitor address: {{ config.monitor.address }}</div>
-              </v-flex>
+                <v-flex md="6">Build Mode: {{ config.buildMode }}</v-flex>
 
-              <v-flex v-if="config.deploy" md2 pa-2>
-                <div v-if="editing === item.id">
-                  Replicas:
-                  <v-text-field label :rules="rules.notzero" v-model="config.deploy.replicas"></v-text-field>
-                </div>
-                <div v-else>Replicas: {{ config.deploy.replicas }}</div>
-            </v-flex>-->
+                <v-flex md="6">K8s Template Version: {{ config.configVer }}</v-flex>
+
+                <v-flex md="6">Self-Release Version: {{ config.version }}</v-flex>
+              </v-layout>
+            </div>-->
+
+            <!-- <v-layout column>
+                <v-row>
+                  <v-col>Enable:</v-col>
+                  <v-col>{{ config.enable }}</v-col>
+                </v-row>
+
+                <v-row>
+                  <v-col>Dev Branch:</v-col>
+                  <v-col>{{ config.devBranch }}</v-col>
+                </v-row>
+                <v-row>
+                  <v-col>Build Mode:</v-col>
+                  <v-col>{{ config.buildMode }}</v-col>
+                </v-row>
+                <v-row>
+                  <v-col>K8s Template Version:</v-col>
+                  <v-col>{{ config.configVer }}</v-col>
+                </v-row>
+                <v-row>
+                  <v-col>Self-Release Version:</v-col>
+                  <v-col>{{ config.version }}</v-col>
+                </v-row>
+            </v-layout>-->
+
             <v-card-actions>
               <div class="flex-grow-1"></div>
 
@@ -189,6 +125,21 @@
         <v-container v-else>fetching...</v-container>
       </v-card>
     </v-dialog>
+
+    <v-flex>
+      <v-snackbar
+        v-if="notify"
+        v-model="notify"
+        :bottom="true"
+        :color="notify.color"
+        :multi-line="true"
+        :right="true"
+        :timeout="notify.timeout"
+      >
+        {{ notify.msg }}
+        <v-btn dark text @click="notify = false">Close</v-btn>
+      </v-snackbar>
+    </v-flex>
   </v-row>
 </template>
 
@@ -207,7 +158,13 @@ export default {
       config: {
         id: "1"
       },
+      _config: null,
       dialog: false,
+      notify: null,
+
+      buildmodes: ["auto", "on", "manual"],
+      configvers: ["phpv1"],
+      vers: ["v1.0.0"],
 
       error: false,
       submitting: false,
@@ -229,7 +186,6 @@ export default {
           v => /^\d+$/.test(v) || "must be number"
         ]
       },
-      creating: null,
       editing: null
     };
   },
@@ -243,11 +199,24 @@ export default {
         .then(json => json.data)
         .then(json => {
           this.config = json.selfrelease;
+          // this.setdefault();
+          this._config = JSON.parse(JSON.stringify(this.config));
         })
         .catch(err => {
           // console.log("getinfos err", err);
           this.notify = { color: "error", msg: err.message, timeout: 86400 };
         });
+    },
+    setdefault() {
+      if (!this.config.version) {
+        this.config.version = "v1.0.0";
+      }
+      if (!this.config.configVer) {
+        this.config.configVer = "phpv1";
+      }
+      if (!this.config.buildMode) {
+        this.config.buildMode = "auto";
+      }
     },
     editMode(item) {
       this.cacheditem = Object.assign({}, item);
@@ -261,17 +230,65 @@ export default {
 
     // form
     handleSubmit(item) {
+      // if (this.enable) {
+      //   item.enable = "on";
+      // } else {
+      //   item.enable = "off";
+      // }
+      let a = {
+        selfrelease: item
+      };
+      let data = JSON.stringify(a);
       // if (this.$refs.form.validate()) {
       //   this.snackbar = true;
       //   console.log("not valid for config");
       // }
 
+      // item.nodePort = parseInt(item.nodePort, 10);
+      // item.deploy.replicas = parseInt(item.deploy.replicas, 10);
+      // this.$emit("add:item", item);
+
+      if (JSON.stringify(this._config) == JSON.stringify(this.config)) {
+        this.notify = { color: "orange", msg: "there's no change" };
+        console.log("no need update all");
+        return;
+      }
+
       this.clearStatus();
+
+      // prevent submitting during submitting
       this.submitting = true;
 
-      item.nodePort = parseInt(item.nodePort, 10);
-      item.deploy.replicas = parseInt(item.deploy.replicas, 10);
-      this.$emit("add:item", item);
+      // let a = convertback(this.all);
+      let url = "/api/projects/" + this.project.name + "/config";
+      // let data = JSON.stringify(a, replacer, 2);
+
+      console.log("data:", data);
+
+      // call api
+      this.loading = true;
+      this.$POST(url, data)
+        .then(res => {
+          console.log("done submit");
+          console.log("submit result", res);
+          this.loading = false;
+          this.submitting = false;
+          this.updated = false;
+          this.notify = {
+            color: "success",
+            msg: "all saved",
+            timeout: 50000
+          };
+          // deep copy instead
+          this._config = JSON.parse(JSON.stringify(this.config));
+        })
+        .catch(err => {
+          console.log("submit all err", err);
+          this.loading = false;
+          this.submitting = false;
+          this.updated = false;
+          this.notify = { color: "error", msg: err.message, timeout: 86400 };
+        });
 
       this.success = true;
       this.error = false;
